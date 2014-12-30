5d683c464aade0e56c5efc7c471843d4fcc35e1d
Aurélien Noce <aurelien.noce@imagine-app.fr>
2014-12-30 14:19:17 +0100

    Factorize test

--- spec/url_signer_spec.rb
+++ spec/url_signer_spec.rb
@@ -4,34 +4,24 @@ describe UrlSigner do
   let(:url) { 'http://google.fr' }
   let(:fake_signer) { double(sign: true, valid?: true) }
 
-  describe ".sign:" do
+  FORWARDED_METHOD = %i(sign valid?)
+  FORWARDED_METHOD.each do |method|
+    describe ".#{method}:" do
 
-    it "builds a new signer" do
-      expect(UrlSigner::Signer).to receive(:new).with(key: 'toto').and_return(fake_signer)
-      UrlSigner.sign(url, key: 'toto')
+      it "builds a new signer" do
+        expect(UrlSigner::Signer).to receive(:new).with(key: 'toto').and_return(fake_signer)
+        UrlSigner.sign(url, key: 'toto')
+      end
+
+      it "calls ##{method} on the new signer" do
+        allow(UrlSigner::Signer).to receive(:new).and_return(fake_signer)
+        expect(fake_signer).to receive(method).with(url)
+
+        # call method by name dynamically
+        UrlSigner.send(method, url, key: 'toto')
+      end
+
     end
-
-    it "calls #sign on the new signer" do
-      allow(UrlSigner::Signer).to receive(:new).and_return(fake_signer)
-      expect(fake_signer).to receive(:sign).with(url)
-      UrlSigner.sign(url, key: 'toto')
-    end
-
-  end
-
-  describe ".valid?:" do
-
-    it "builds a new signer" do
-      expect(UrlSigner::Signer).to receive(:new).with(key: 'toto').and_return(fake_signer)
-      UrlSigner.sign(url, key: 'toto')
-    end
-
-    it "calls #valid? on the new signer" do
-      allow(UrlSigner::Signer).to receive(:new).and_return(fake_signer)
-      expect(fake_signer).to receive(:sign).with(url)
-      UrlSigner.sign(url, key: 'toto')
-    end
-
   end
 
 end
